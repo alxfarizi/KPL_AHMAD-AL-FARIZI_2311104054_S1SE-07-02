07fb34dc68492d7643d2f16cd0dbd65a7e897075
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2025-03-08 23:06:31 +0700

    refactor: Rename folder

--- 04_Automata_dan_Table-Driven_Construction/TP4/TP4/Program.cs
+++ 04_Automata_dan_Table-Driven_Construction/TP4/TP4/Program.cs
@@ -4,14 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Jalankan KodePos
         KodePos kodePos = new KodePos();
         kodePos.getKodePos();
 
-        // Jalankan DoorMachine
-        DoorMachine door = new DoorMachine(); // State awal: Terkunci
-        door.ChangeState("Terbuka");  // Output: Pintu tidak terkunci
-        door.ChangeState("Terkunci"); // Output: Pintu terkunci
+        DoorMachine door = new DoorMachine(); 
+        door.ChangeState("Terbuka");  
 
         // Menahan console agar tidak langsung tertutup
         Console.WriteLine("\nTekan Enter untuk keluar...");
