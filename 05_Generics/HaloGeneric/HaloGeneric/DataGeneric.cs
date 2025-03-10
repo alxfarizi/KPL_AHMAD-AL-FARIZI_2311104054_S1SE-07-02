eae7e259f8b59fdfd8179eb5f33f951001964f04
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2025-03-10 18:40:06 +0700

    feat: menambahkan class DataGeneric

---
+++ 05_Generics/HaloGeneric/HaloGeneric/DataGeneric.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Internal;
+
+namespace tpmodul5_2311104054
+{
+	public class DataGeneric
+	{
+        private T data; // Field privat untuk menyimpan data
+
+        // Constructor untuk inisialisasi data
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        // Method untuk mencetak data ke konsol
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah: {data}");
+        }
+    }
+}
+
---
+++ 05_Generics/HaloGeneric/HaloGeneric/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        DataGeneric<string> data = new DataGeneric<string>("12345678"); 
+        data.PrintData(); 
+    }
+}
