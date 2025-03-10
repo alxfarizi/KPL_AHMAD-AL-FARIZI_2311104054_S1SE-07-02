6e9d2674fb62b4278ee8bf561a998fb314cf0bff
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2025-03-10 18:33:00 +0700

    feat: menambahkan class HaloGeneric dengan method SapaUser

---
+++ 05_Generics/HaloGeneric/HaloGeneric/HaloGeneric.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace tpmodul5_2311104054
+{
+    public class HaloGeneric<T>
+    {
+        private T user; 
+
+        public HaloGeneric(T user)
+        {
+            this.user = user;
+        }
+
+        public void SapaUser()
+        {
+            Console.WriteLine($"Halo user {user}!");
+        }
+    }
+
+}
+
---
+++ 05_Generics/HaloGeneric/HaloGeneric/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using tpmodul5_2311104054;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric<string> halo = new HaloGeneric<string>("Fariz"); 
+        halo.SapaUser();
+    }
+}
