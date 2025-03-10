4911b6d507fd8768bdbc8c8f936cd59e771a2f47
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2025-03-10 17:48:03 +0700

    feat: inisiasi class Program.cs

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_CSharp/tpmodul2_2311104054/tpmodul2_2311104054/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Masukkan satu huruf: ");
+        char input = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        if ("AIUEO".Contains(input))
+        {
+            Console.WriteLine($"Huruf {input} merupakan huruf vokal");
+        }
+        else
+        {
+            Console.WriteLine($"Huruf {input} merupakan huruf konsonan");
+        }
+
+        int[] bilanganGenap = { 2, 4, 6, 8, 10 };
+
+        for (int i = 0; i < bilanganGenap.Length; i++)
+        {
+            Console.WriteLine($"Angka genap {i + 1} : {bilanganGenap[i]}");
+        }
+    }
+}
