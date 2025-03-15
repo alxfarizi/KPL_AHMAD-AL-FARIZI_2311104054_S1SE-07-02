acd5359428f05aefab1c51c4d672337589263fac
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2025-03-15 11:40:42 +0700

    feat: menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ 05_Generics/Jurnal/modul5_2311104054/modul5_2311104054/Penjumlahan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace modul5_2311104054
+{
+	public class Penjumlahan
+	{
+        public T JumlahTigaAngka<T>(T angka1, T angka2, T angka3) where T : struct
+        {
+            dynamic a = angka1;
+            dynamic b = angka2;
+            dynamic c = angka3;
+
+            return (T)(a + b + c);
+        }
+    }
+}
+
