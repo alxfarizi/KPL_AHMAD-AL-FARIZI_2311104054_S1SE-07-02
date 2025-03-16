e3b7e81f15095fa43739be06afa04f57aff7a903
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2025-03-16 15:36:26 +0700

    feat: menambahkan class SimpleDataBase

--- 05_Generics/Jurnal/modul5_2311104054/modul5_2311104054/Program.cs
+++ 05_Generics/Jurnal/modul5_2311104054/modul5_2311104054/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using modul5_2311104054;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        SimpleDataBase<int> database = new SimpleDataBase<int>();
+
+        database.AddNewData(12);
+        database.AddNewData(34);
+        database.AddNewData(56);
+
+        database.PrintAllData();
+    }
+}
