c73db19c5d378f26eb570863eae95754562207c0
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2025-03-18 19:17:03 +0700

    chore: menambahkan class DataGeneric

--- 05_Generics/TP/tpmodul5_2311104054/tpmodul5_2311104054/Program.cs
+++ 05_Generics/TP/tpmodul5_2311104054/tpmodul5_2311104054/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using tpmodul5_2311104054;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataGeneric<string> data = new DataGeneric<string>("Ahmad Al - Farizi");
+        data.PrintData();
+    }
+}
