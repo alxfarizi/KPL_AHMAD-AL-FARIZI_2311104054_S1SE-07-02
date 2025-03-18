1c4af39fc9ff4b7fbf25f6feca7a395ce5602410
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2025-03-18 15:51:35 +0700

    feat: menambahkan class HaloGeneric dengan method SapaUser

---
+++ 05_Generics/TP/tpmodul5_2311104054/tpmodul5_2311104054/HaloGeneric.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace tpmodul5_2311104054
+{
+	public class HaloGeneric
+	{
+        public void SapaUser<T>(T input)
+        {
+            Console.WriteLine($"Halo user {input}"); // Mencetak sapaan dengan input
+        }
+    }
+}
+
---
+++ 05_Generics/TP/tpmodul5_2311104054/tpmodul5_2311104054/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using tpmodul5_2311104054;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Masukkan nama pengguna: ");
+        string inputUser = Console.ReadLine();
+
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser(inputUser); 
+    }
+}
