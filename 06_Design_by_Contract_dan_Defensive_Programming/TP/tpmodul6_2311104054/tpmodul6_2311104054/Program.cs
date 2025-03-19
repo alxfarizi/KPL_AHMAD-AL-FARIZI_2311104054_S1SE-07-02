994210d6036ee8c578c6fecf5b4284f3c9ff84c2
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2025-03-19 11:30:54 +0700

    feat: menambahkan class SayaTubeVideo

---
+++ 06_Design_by_Contract_dan_Defensive_Programming/TP/tpmodul6_2311104054/tpmodul6_2311104054/Program.cs
@@ -0,0 +1,11 @@
+﻿using tpmodul6_2311104054;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – [Ahmad Al - Farizi]");
+        video.IncreasePlayCount(100);
+        video.PrintVideoDetails();
+    }
+}
---
+++ 06_Design_by_Contract_dan_Defensive_Programming/TP/tpmodul6_2311104054/tpmodul6_2311104054/SayaTubeVideo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace tpmodul6_2311104054
+{
+	public class SayaTubeVideo
+	{
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            Random random = new Random();
+            this.id = random.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            this.playCount += count;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($"ID: {this.id}");
+            Console.WriteLine($"Title: {this.title}");
+            Console.WriteLine($"Play Count: {this.playCount}");
+        }
+    }
+}
+
