f06b8ccfa392daf59f2fdca28c70dd53a1dd8cbe
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2025-03-23 12:06:56 +0700

    feat: menambahkan dua class SayaTubeVideo dan SayaTubeUser

---
+++ 06_Design_by_Contract_dan_Defensive_Programming/Jurnal/modul6_2311104054/modul6_2311104054/SayaTubeUser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using modul6_2311104054;
+
+public class SayaTubeUser
+{
+    private int id;
+    private string username;
+    private List<SayaTubeVideo> uploadedVideos;
+
+    public SayaTubeUser(string username)
+    {
+        Random random = new Random();
+        this.id = random.Next(10000, 99999);
+
+        this.username = username;
+        this.uploadedVideos = new List<SayaTubeVideo>();
+    }
+
+    public int GetTotalVideoPlayCount()
+    {
+        int total = 0;
+        foreach (var video in uploadedVideos)
+        {
+            total += video.GetPlayCount();
+        }
+        return total;
+    }
+
+    public void AddVideo(SayaTubeVideo video)
+    {
+        uploadedVideos.Add(video);
+    }
+
+    public void PrintAllVideoPlaycount()
+    {
+        Console.WriteLine($"User: {username}");
+        int i = 1;
+        foreach (var video in uploadedVideos)
+        {
+            Console.WriteLine($"Video {i} judul: {video.GetTitle()}");
+            i++;
+        }
+    }
+}
---
+++ 06_Design_by_Contract_dan_Defensive_Programming/Jurnal/modul6_2311104054/modul6_2311104054/SayaTubeVideo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using modul6_2311104054;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random random = new Random();
+        this.id = random.Next(10000, 99999);
+
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        this.playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine($"ID: {id}");
+        Console.WriteLine($"Title: {title}");
+        Console.WriteLine($"Play Count: {playCount}");
+    }
+
+    public int GetPlayCount()
+    {
+        return playCount;
+    }
+
+    public string GetTitle()
+    {
+        return title;
+    }
+}
