059e69f452f2463a2dfba4b818418d7e166a205b
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2025-03-23 11:05:49 +0700

    feat: menambahkan class Program.cs

---
+++ 06_Design_by_Contract_dan_Defensive_Programming/Praktikum/praktikum6/praktikum6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+public class ExceptionTest
+{
+    static double SafeDivision(double x, double y)
+    {
+        if (y == 0)
+            return x / y;
+        throw new DivideByZeroException();
+    }
+    public static void Main()
+    {
+        double a = 98, b = 0;
+        double result;
+        try
+        {
+            result = SafeDivision(a, b);
+            Console.WriteLine("{0} divided by {1} = {2}", a, b,
+            result);
+
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Attempted divide by zero.");
+        }
+    }
+}
