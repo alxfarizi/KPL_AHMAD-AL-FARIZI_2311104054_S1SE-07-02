6e9ccc098d562c77f8b8524b23937d2d2ab41a60
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2025-03-25 11:31:14 +0700

    feat: penambahan design by contract pada semua kelas

--- 06_Design_by_Contract_dan_Defensive_Programming/Jurnal/modul6_2311104054/modul6_2311104054/SayaTubeUser.cs
+++ 06_Design_by_Contract_dan_Defensive_Programming/Jurnal/modul6_2311104054/modul6_2311104054/SayaTubeUser.cs
@@ -9,6 +9,9 @@ public class SayaTubeUser
 
     public SayaTubeUser(string username)
     {
+        if (username == null) throw new ArgumentNullException("Username tidak boleh null.");
+        if (username.Length > 100) throw new ArgumentException("Panjang username tidak boleh lebih dari 100 karakter.");
+
         Random random = new Random();
         this.id = random.Next(10000, 100000); 
         this.username = username;
@@ -17,6 +20,9 @@ public class SayaTubeUser
 
     public void AddVideo(SayaTubeVideo video)
     {
+        if (video == null) throw new ArgumentNullException("Video tidak boleh null.");
+        if (video.GetPlayCount() > int.MaxValue) throw new ArgumentException("Play count pada video melebihi batas integer maksimum.");
+
         uploadedVideos.Add(video);
     }
 
@@ -34,8 +40,10 @@ public class SayaTubeUser
     {
         Console.WriteLine("User: " + username);
         int i = 1;
+
         foreach (var video in uploadedVideos)
         {
+            if (i > 8) break; 
             Console.WriteLine("Video " + i + " judul: " + video.GetTitle());
             i++;
         }
--- 06_Design_by_Contract_dan_Defensive_Programming/Jurnal/modul6_2311104054/modul6_2311104054/SayaTubeVideo.cs
+++ 06_Design_by_Contract_dan_Defensive_Programming/Jurnal/modul6_2311104054/modul6_2311104054/SayaTubeVideo.cs
@@ -8,6 +8,9 @@ public class SayaTubeVideo
 
     public SayaTubeVideo(string title)
     {
+        if (title == null) throw new ArgumentNullException("Title tidak boleh null.");
+        if (title.Length > 200) throw new ArgumentException("Panjang title tidak boleh lebih dari 200 karakter.");
+
         Random random = new Random();
         this.id = random.Next(10000, 100000); 
         this.title = title;
@@ -16,7 +19,20 @@ public class SayaTubeVideo
 
     public void IncreasePlayCount(int count)
     {
-        this.playCount += count;
+        if (count < 0) throw new ArgumentException("Play count tidak boleh negatif.");
+        if (count > 25000000) throw new ArgumentException("Play count maksimal adalah 25.000.000.");
+
+        try
+        {
+            checked
+            {
+                this.playCount += count;
+            }
+        }
+        catch (OverflowException ex)
+        {
+            Console.WriteLine("Error: Overflow terjadi saat menambahkan play count. " + ex.Message);
+        }
     }
 
     public void PrintVideoDetails()
