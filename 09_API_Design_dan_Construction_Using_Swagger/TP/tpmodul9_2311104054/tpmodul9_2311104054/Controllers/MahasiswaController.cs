256b971374b4f83236024f368ca448e9caf35f23
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2025-04-24 21:55:11 +0700

    feat: menambahkan API mahasiswa

---
+++ 09_API_Design_dan_Construction_Using_Swagger/TP/tpmodul9_2311104054/tpmodul9_2311104054/Controllers/MahasiswaController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MahasiswaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> mahasiswas = new List<Mahasiswa>
+        {
+            new Mahasiswa { Nama = "Fariz", Nim = "1302000001" },
+            new Mahasiswa { Nama = "Kafka", Nim = "1302000002" },
+            new Mahasiswa { Nama = "Farhan", Nim = "1302000003" }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Mahasiswa>> Get()
+        {
+            return mahasiswas;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Mahasiswa> Get(int id)
+        {
+            if (id >= 0 && id < mahasiswas.Count)
+            {
+                return mahasiswas[id];
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa value)
+        {
+            mahasiswas.Add(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            if (id >= 0 && id < mahasiswas.Count)
+            {
+                mahasiswas.RemoveAt(id);
+            }
+        }
+    }
+
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+    }
+}
