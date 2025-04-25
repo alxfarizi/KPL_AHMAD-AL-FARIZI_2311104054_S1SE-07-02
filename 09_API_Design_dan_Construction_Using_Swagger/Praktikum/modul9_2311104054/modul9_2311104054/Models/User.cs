7cce50fe218edf52bb02f66dcae764c1e8c21d97
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2025-04-25 10:30:40 +0700

    feat: menambahkan class User dan class UserDto

---
+++ 09_API_Design_dan_Construction_Using_Swagger/Praktikum/modul9_2311104054/modul9_2311104054/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace modul9_2311104054.Models
+{
+	public class User
+	{
+		public int id { get; set; }
+		public string name { get; set; }
+		public string email { get; set; }
+	}
+}
+
---
+++ 09_API_Design_dan_Construction_Using_Swagger/Praktikum/modul9_2311104054/modul9_2311104054/Models/UserDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace modul9_2311104054.Models
+{
+	public class UserDto
+	{
+        public string name { get; set; }
+        public string email { get; set; }
+    }
+}
+
