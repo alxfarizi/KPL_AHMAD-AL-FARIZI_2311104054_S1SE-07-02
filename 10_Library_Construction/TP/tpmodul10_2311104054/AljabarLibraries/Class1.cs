4bb705f3befd57c7b4fb8a6c3adcdf44f36fc83d
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2025-05-02 21:24:45 +0700

    feat: Membuat project dan library aljabar

---
+++ 10_Library_Construction/TP/tpmodul10_2311104054/AljabarLibraries/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AljabarLibraries
+{
+    public class Aljabar
+    {
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            if (persamaan.Length != 3)
+                throw new ArgumentException("Persamaan harus memiliki 3 elemen: a, b, dan c.");
+
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+
+            double diskriminan = (b * b) - (4 * a * c);
+
+            if (diskriminan < 0)
+                throw new ArgumentException("Persamaan tidak memiliki akar real.");
+
+            double akar1 = (-b + Math.Sqrt(diskriminan)) / (2 * a);
+            double akar2 = (-b - Math.Sqrt(diskriminan)) / (2 * a);
+
+            return new double[] { akar1, akar2 };
+        }
+
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            if (persamaan.Length != 2)
+                throw new ArgumentException("Persamaan harus memiliki 2 elemen: a dan b.");
+
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            double aKuadrat = a * a;
+            double duaAb = 2 * a * b;
+            double bKuadrat = b * b;
+
+            return new double[] { aKuadrat, duaAb, bKuadrat };
+        }
+    }
+}
---
+++ 10_Library_Construction/TP/tpmodul10_2311104054/ShowCase/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AljabarLibraries;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double[] persamaanKuadrat = { 1, -3, -10 };
+                double[] akar = Aljabar.AkarPersamaanKuadrat(persamaanKuadrat);
+                Console.WriteLine("Akar-akar persamaan kuadrat: {0}, {1}", akar[0], akar[1]);
+
+                double[] persamaanLinear = { 2, -3 };
+                double[] hasilKuadrat = Aljabar.HasilKuadrat(persamaanLinear);
+                Console.WriteLine("Hasil kuadrat persamaan: {0}, {1}, {2}",
+                                  hasilKuadrat[0], hasilKuadrat[1], hasilKuadrat[2]);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Terjadi kesalahan: " + e.Message);
+            }
+        }
+    }
+}
