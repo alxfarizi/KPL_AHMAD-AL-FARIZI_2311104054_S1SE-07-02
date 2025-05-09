4a0ef60a72ba6cd35b29ce84dfd5c509c8a2d66d
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2025-05-09 20:27:04 +0700

    feat: Membuat project dan library matematika

---
+++ 10_Library_Construction/Jurnal/modul10_2311104054/Matematika/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MatematikaLibraries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("================ Jurnal Modul 10 ======================");
+        Console.WriteLine("FPB(60, 45)             : " + Matematika.FPB(60, 45));
+
+        Console.WriteLine("KPK(12, 8)              : " + Matematika.KPK(12, 8));
+
+        int[] persamaanTurunan = { 1, 4, -12, 9 }; 
+        Console.WriteLine("Turunan({1, 4, -12, 9}) : " + Matematika.Turunan(persamaanTurunan));
+
+        int[] persamaanIntegral = { 4, 6, -12, 9 }; 
+        Console.WriteLine("Integral({4, 6, -12, 9}): " + Matematika.Integral(persamaanIntegral));
+        Console.WriteLine("=======================================================");
+    }
+}
---
+++ 10_Library_Construction/Jurnal/modul10_2311104054/MatematikaLibraries/MatematikaApps.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace MatematikaLibraries
+{
+    public class Matematika
+    {
+        public static int FPB(int input1, int input2)
+        {
+            while (input2 != 0)
+            {
+                int temp = input2;
+                input2 = input1 % input2;
+                input1 = temp;
+            }
+            return input1;
+        }
+
+        public static int KPK(int input1, int input2)
+        {
+            return (input1 * input2) / FPB(input1, input2);
+        }
+
+        public static string Turunan(int[] persamaan)
+        {
+            string hasil = "";
+            for (int i = 0; i < persamaan.Length - 1; i++)
+            {
+                int koefisien = persamaan[i] * (persamaan.Length - 1 - i);
+                int pangkat = persamaan.Length - 2 - i;
+
+                if (koefisien != 0)
+                {
+                    if (hasil != "" && koefisien > 0)
+                        hasil += " + ";
+                    else if (koefisien < 0)
+                        hasil += " - ";
+
+                    hasil += Math.Abs(koefisien);
+                    if (pangkat > 0)
+                    {
+                        hasil += "x";
+                        if (pangkat > 1)
+                            hasil += pangkat;
+                    }
+                }
+            }
+            return hasil == "" ? "0" : hasil;
+        }
+
+        public static string Integral(int[] persamaan)
+        {
+            string hasil = "";
+            for (int i = 0; i < persamaan.Length; i++)
+            {
+                int pangkat = persamaan.Length - i;
+                int koefisien = persamaan[i];
+
+                if (koefisien != 0)
+                {
+                    if (hasil != "" && koefisien > 0)
+                        hasil += " + ";
+                    else if (koefisien < 0)
+                        hasil += " - ";
+
+                    hasil += Math.Abs(koefisien) + "/" + pangkat + "x";
+                    if (pangkat > 1)
+                        hasil += pangkat;
+
+
+                }
+            }
+            return hasil;
+
+        }
+    }
+}
