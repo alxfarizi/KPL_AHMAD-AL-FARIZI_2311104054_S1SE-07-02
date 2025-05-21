c17e31c6ea86153a60c88d2f1c4bd1f2384827bd
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2025-05-21 23:38:48 +0700

    feat: implementasi design pattern Singleton

---
+++ 13_Design_Pattern_Implementation/Jurnal/modul13_2311104054/modul13_2311104054/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace modul13_2311104054
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PusatDataSingleton data1 = PusatDataSingleton.GetDataSingleton();
+            PusatDataSingleton data2 = PusatDataSingleton.GetDataSingleton();
+
+            data1.AddSebuahData("Anggota 1 - Farhan");
+            data1.AddSebuahData("Anggota 2 - Shafiq");
+            data1.AddSebuahData("Asisten - Al-Farizi");
+
+            Console.WriteLine("Data dari data2:");
+            data2.PrintSemuaData();
+
+            data2.HapusSebuahData(2);
+
+            Console.WriteLine("\nData dari data1 setelah penghapusan:");
+            data1.PrintSemuaData();
+
+            Console.WriteLine($"\nJumlah data di data1: {data1.GetSemuaData().Count}");
+            Console.WriteLine($"Jumlah data di data2: {data2.GetSemuaData().Count}");
+        }
+    }
+}
