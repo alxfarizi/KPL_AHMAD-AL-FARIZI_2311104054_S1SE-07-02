398a231370d770eee827ca9de6a7b42067528bd3
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2025-06-01 19:35:34 +0700

    style: tidak ada pembaruan

--- 02_Pengenalan_IDE_dan_Pemrograman_CSharp/modul2_2311104054/modul2_2311104054/obj/Debug/net7.0/modul2_2311104054.AssemblyInfo.cs
+++ 02_Pengenalan_IDE_dan_Pemrograman_CSharp/modul2_2311104054/modul2_2311104054/obj/Debug/net7.0/modul2_2311104054.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul2_2311104054")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f55f7bdc8e1ca08a33fcd2e66a836c92cd48712c")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul2_2311104054")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul2_2311104054")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
