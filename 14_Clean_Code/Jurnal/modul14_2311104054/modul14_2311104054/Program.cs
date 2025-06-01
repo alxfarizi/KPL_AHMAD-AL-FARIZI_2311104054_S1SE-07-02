7b90acf40c27bf4228ac4db71aae7a65173d990f
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2025-06-01 20:50:15 +0700

    feat: implementasi clean code dari Jurnal 2

---
+++ 14_Clean_Code/Jurnal/modul14_2311104054/modul14_2311104054/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Implementasi dari Modul Jurnal 2");
+        Console.WriteLine("");
+        Console.Write("Masukkan nama Anda: ");
+        string nama = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine($"Selamat datang, {nama}!");
+        Console.WriteLine();
+
+        int[] angkaArray = new int[7];
+
+        for (int i = 0; i < angkaArray.Length; i++)
+        {
+            angkaArray[i] = i;
+
+            if (i % 2 == 0 && i % 3 == 0)
+            {
+                Console.WriteLine($"{angkaArray[i]} #$#$");
+            }
+            else if (i % 2 == 0)
+            {
+                Console.WriteLine($"{angkaArray[i]} ##");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine($"{angkaArray[i]} $$");
+            }
+            else
+            {
+                Console.WriteLine($"{angkaArray[i]}");
+            }
+        }
+
+        Console.WriteLine();
+        Console.Write("Masukkan angka (1-10000): ");
+       
+        string inputNilai = Console.ReadLine();
+        int nilai = Convert.ToInt32(inputNilai);
+
+        Console.WriteLine();
+
+        if (ApakahBilanganPrima(nilai))
+        {
+            Console.WriteLine($"Angka {nilai} merupakan bilangan prima.");
+        }
+        else
+        {
+            Console.WriteLine($"Angka {nilai} bukan merupakan bilangan prima.");
+        }
+    }
+
+    /// <summary>
+    /// Mengecek apakah sebuah bilangan adalah bilangan prima.
+    /// </summary>
+    /// <param name="angka">Bilangan yang akan dicek.</param>
+    /// <returns>true jika bilangan prima, false jika bukan.</returns>
+    static bool ApakahBilanganPrima(int angka)
+    {
+        if (angka <= 1)
+            return false;
+
+        for (int i = 2; i <= Math.Sqrt(angka); i++)
+        {
+            if (angka % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
