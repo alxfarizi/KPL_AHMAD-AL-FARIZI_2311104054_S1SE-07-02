c3853bc2894f14aafb5a04d32037051d75f29fd6
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2025-06-01 15:53:10 +0700

    feat: implementasi clean code dari TP 2

---
+++ 14_Clean_Code/TP/tpmodul14_2311104054/tpmodul14_2311104054/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Implementasi dari TP Modul 2");
+        Console.WriteLine("");
+        // Input karakter dari pengguna
+        Console.Write("Masukkan satu huruf: ");
+        char inputChar = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+        Console.WriteLine("");
+
+        // Mengecek apakah karakter merupakan huruf vokal
+        if ("AIUEO".Contains(inputChar))
+        {
+            Console.WriteLine($"Huruf {inputChar} merupakan huruf vokal");
+            Console.WriteLine("");
+        }
+        else
+        {
+            Console.WriteLine($"Huruf {inputChar} merupakan huruf konsonan");
+            Console.WriteLine("");
+        }
+
+        // Array bilangan genap
+        int[] evenNumbers = { 2, 4, 6, 8, 10 };
+
+        // Menampilkan setiap elemen dalam array
+        for (int index = 0; index < evenNumbers.Length; index++)
+        {
+            Console.WriteLine($"Angka genap {index + 1}: {evenNumbers[index]}");
+        }
+    }
+}
