d6d01e4f410a7673cb752ca8269e914d03c7c542
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2025-06-13 21:46:36 +0700

    feat: menambahkan laporan praktikum

--- 15_Review_Tugas_Besar/Jurnal/modul15_2311104054/modul15_2311104054/obj/Debug/net7.0/modul15_2311104054.AssemblyInfo.cs
+++ 15_Review_Tugas_Besar/Jurnal/modul15_2311104054/modul15_2311104054/obj/Debug/net7.0/modul15_2311104054.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul15_2311104054")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2e9160391780ad9d974d03eb316dcb2306372a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul15_2311104054")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul15_2311104054")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
